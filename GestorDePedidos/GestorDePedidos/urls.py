a812ae39625a9afc057baa2d07d85da3949a2834
Agustín Mayer <mayeragustin99@gmail.com>
2024-03-09 16:21:26 -0300

    algunos cambios

--- GestorDePedidos/GestorDePedidos/urls.py
+++ GestorDePedidos/GestorDePedidos/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.site.site_header = "Gestor de pedidos"
+admin.site.site_title = "Gestor de pedidos"
+admin.site.index_title = "Sitio de gestion de pedidos"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pedidos/', include("pedidos.urls")),
 ]
---
+++ GestorDePedidos/pedidos/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from django.shortcuts import render
+from .models import Pedido
+
+# Create your views here.
+def index(request):
+    return render(request, "pedidos/index.html", {
+        "pedidos": Pedido.objects.all()
+    })
+
+def pedido(request, id_pedido):
+    try:
+        pedido = Pedido.objects.get(id=id_pedido)
+        return render(request, "pedidos/pedido.html", {
+            "pedido": pedido,
+            "pasajeros": pedido.clientes.all()
+        })
+    except Pedido.DoesNotExist:
+        return HttpResponseRedirect(reverse("index"))
