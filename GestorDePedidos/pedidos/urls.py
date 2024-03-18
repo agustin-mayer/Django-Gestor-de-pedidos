72a3dd9da17ecee64301de0f9ca50943e90814de
Agustín Mayer <mayeragustin99@gmail.com>
2024-03-18 00:24:48 -0300

    Update urls.py

--- GestorDePedidos/pedidos/urls.py
+++ GestorDePedidos/pedidos/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path("<int:id_pedido>", views.pedido, name="pedido"),
+    path('crear_pedido/', views.crear_pedido, name="crear_pedido"),
+    path("modificar_pedido/<int:pedido_id>", views.modificar_pedido, name="modificar_pedido"),
+    path('crear_cliente/', views.crear_cliente, name="crear_cliente"),
+    path('eliminar_pedido/<int:pedido_id>/', views.eliminar_pedido, name='eliminar_pedido')
 ]
